ca906ac486ecf45b67b09006d01ae2e6ff7b9833
UTENTE 14 <UTENTE 14@PC14-LAB3>
2023-12-13 09:46:03 +0100

    Aggiungere i file di progetto.

---
+++ AgeOfWar/Cavaliere.cs
@@ -0,0 +1,19 @@
+﻿using AgeOfWar.Personaggi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeOfWar.Personaggi
+{
+    public class Cavaliere: Personaggio
+    {
+        public Cavaliere(int puntiForza)
+        {
+            PuntiVita = 1000;
+            PuntiForza += puntiForza;
+        }
+       
+    }
+}
---
+++ AgeOfWar/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AgeOfWar.Personaggi;
+
+Cavaliere cav1 = new Cavaliere(200);
+
+Cavaliere[] SquadraRossa = new Cavaliere[100];
+for (int i = 1; i <= 100; i++)
+{
+    SquadraRossa[i] = new Cavaliere(200);
+    SquadraRossa[i].Squadra = "Rossi";
+}
